dda574126958a171e178cc6e9e16e0c29d6fd806
guemax <github-mg@guenther.cloud>
2023-01-15 09:20:38 +0100

    Resolve violations of current PEP 8 styling conventions

--- scripts/merge_comp_freqs.py
+++ scripts/merge_comp_freqs.py
@@ -1,13 +1,13 @@
-## Merges two CSV documents.
-##
-
+"""Merges two CSV documents"""
 import csv
 import sys
 import os
 
+
 class MergeError(Exception):
 	def __init__(self, message):
 		Exception.__init__(self, message)
+
 
 class Merged(object):
 	def __init__(self):
@@ -26,21 +26,25 @@ class Merged(object):
 
 	def record_detected(self, name, complexity):
 		self._all_modules_with_complexity[name] = complexity
-	
+
+
 def skip_heading(f):
 	next(f)
-	
+
+
 def read_heading_from(r):
 	p = r.next()
-	while p == []:
+	while not p:
 		p = r.next()
 	return p
 
+
 def validate_content_by(heading, expected):
 	comparison = expected.split(',')
-	stripped = heading[0:len(comparison)] # allow extra fields
+	stripped = heading[0:len(comparison)]  # allow extra fields
 	if stripped != comparison:
 		raise MergeError('Erroneous content. Expected = ' + expected + ', got = ' + ','.join(heading))
+
 
 def parse_csv(merged, filename, parse_action, expected_format):
 	with open(filename, 'rb') as csvfile:
@@ -50,24 +54,29 @@ def parse_csv(merged, filename, parse_action, expected_format):
 		for row in r:
 			parse_action(merged, row)
 
+
 def write_csv(stats):
 	print('module,revisions,code')
 	for s in stats:
-		name, (f,c) = s
+		name, (f, c) = s
 		print(name + ',' + f + ',' + c)
+
 
 def as_os_aware_path(name):
 	return os.path.normpath(name)
-	
+
+
 def parse_complexity(merged, row):
 	name = as_os_aware_path(row[1][2:])
 	complexity = row[4]
 	merged.record_detected(name, complexity)
 
+
 def parse_freqs(merged, row):
 	name = as_os_aware_path(row[0])
 	freqs = row[1]
 	merged.extend_with(name, freqs)
+
 
 def merge(revs_file, comp_file):
 	merged = Merged()
@@ -75,9 +84,14 @@ def merge(revs_file, comp_file):
 	parse_csv(merged, revs_file, parse_freqs, expected_format='entity,n-revs')
 	write_csv(merged.sorted_result())
 
-if __name__ == '__main__':
+
+def main() -> None:
 	if len(sys.argv) != 3:
 		raise MergeError('Wrong arguments. Require one CSV file with frequencies and one with the complexity')
 	revs_file = sys.argv[1]
 	comp_file = sys.argv[2]
 	merge(revs_file, comp_file)
+
+
+if __name__ == '__main__':
+	main()
